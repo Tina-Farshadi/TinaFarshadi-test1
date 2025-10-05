d9e25ff5da15b0fc36c62c15c2d168a3aa30e4a7
Tina-Farshadi <tina.fsh83@gmail.com>
2025-10-05 17:53:31 +0330

    file added

---
+++ main.py
@@ -0,0 +1,13 @@
+
+name = "Tina"
+age = 20
+height = 1.68
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is student?", is_student)
+
+user_name = input("What's your name? ")
+print("Hello,", user_name, "! Nice to meet you.")
